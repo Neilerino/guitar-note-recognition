2647d6d3dc7b0cfebb3339105043d9fbdb08a6cb
Neil Wadden <naw218@mun.ca>
2019-07-07 17:37:02 -0230

    Finished algorithm implementation for detecting frets and strings

--- fretboard/fretboard.py
+++ fretboard/fretboard.py
@@ -1,24 +1,17 @@
 import cv2 as cv
 import numpy as np 
-from neck import Neck
+from neck import Neck, detect_frets, detect_strings, draw_lines
+import math
 
-img = cv.imread(cv.samples.findFile('guitar-fretboard-2.jpg'))
+img = cv.imread('guitar-fretboard-2.jpg')
 
 neck = Neck(img).get()
 gray = cv.cvtColor(neck, cv.COLOR_BGR2GRAY)
-edges = cv.Canny(gray, 50, 150, apertureSize = 3)
 
-lines = cv.HoughLines(edges,1,np.pi/180,200)
-for line in lines:
-    rho,theta = line[0]
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a*rho
-    y0 = b*rho
-    x1 = int(x0 + 1000*(-b))
-    y1 = int(y0 + 1000*(a))
-    x2 = int(x0 - 1000*(-b))
-    y2 = int(y0 - 1000*(a))
-    cv.line(neck,(x1,y1),(x2,y2),(0,0,255),2)
+frets = detect_frets(gray)
+strings = detect_strings(gray)
+
+neck = draw_lines(neck, frets)
+neck = draw_lines(neck, strings)
 
 cv.imwrite('houghlines3.jpg', neck)
--- fretboard/neck.py
+++ fretboard/neck.py
@@ -9,7 +9,7 @@ class Neck:
         self.image = img
         grey = cv.cvtColor(self.image, cv.COLOR_BGR2GRAY)
         self.image = rotate_image(self.image)
-        self.neck = generate_neck(self.image)
+        self.neck = crop_neck(self.image)
 
     def get(self):
         return self.neck
@@ -35,7 +35,7 @@ def rotate_image(img):
 
     return rotate(img, angle)
 
-def generate_neck(img):
+def crop_neck(img):
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     edges = cv.Sobel(gray, cv.CV_8U, 0, 1, 3)
     edges = threshold(edges, 127)
@@ -87,3 +87,44 @@ def rotate(image, angle, center=None, scale=1.0):
     rotated = cv.warpAffine(image, M, (w, h))
 
     return rotated
+
+def detect_frets(img):
+    blur = cv.GaussianBlur(img, (5,5), 0)
+    (thresh, img_bw) = cv.threshold(blur, 100, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+    edges = cv.Sobel(blur, cv.CV_8U, 0, 1, 3)
+    return cv.HoughLines(img_bw, 0.25, np.pi/180, 100)
+    
+
+def detect_strings(img):
+    blur = cv.GaussianBlur(img, (5,5), 0)
+    (thresh, img_bw) = cv.threshold(blur, 100, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+    edges = cv.Sobel(blur, cv.CV_8U, 1, 0, 3)
+    return cv.HoughLines(img_bw, 0.25, np.pi/180, 100)
+
+
+def draw_lines(img, lines):
+    x1_old = y1_old = x2_old = y2_old = 0
+    line_distance = 100
+
+    for line in lines:
+        rho,theta = line[0]
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+
+        if abs(x1 - x1_old) > line_distance \
+        and abs(x2 - x2_old) > line_distance \
+        and abs(y2 - y2_old) > line_distance \
+        and abs(y1 - y1_old) > line_distance:
+            cv.line(img, (x1,y1), (x2,y2), (0,0,255), 3)
+        x1_old = x1
+        x2_old = x2
+        y1_old = y1
+        y2_old = y2
+
+    return img
