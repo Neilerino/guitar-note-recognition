62173c68d117c39a023d3723430310e409c1e36a
Neil Wadden <naw218@mun.ca>
2019-07-07 18:19:07 -0230

    Fretboard detection - first prototype

--- fretboard/neck.py
+++ fretboard/neck.py
@@ -89,42 +89,27 @@ def rotate(image, angle, center=None, scale=1.0):
     return rotated
 
 def detect_frets(img):
+    min_length = 4
+    max_gap = 0
     blur = cv.GaussianBlur(img, (5,5), 0)
     (thresh, img_bw) = cv.threshold(blur, 100, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
-    edges = cv.Sobel(blur, cv.CV_8U, 0, 1, 3)
-    return cv.HoughLines(img_bw, 0.25, np.pi/180, 100)
+    edges = cv.Sobel(img_bw, cv.CV_8U, 1, 0, 5)
+    cv.imwrite('edges.jpg', edges)
+    return cv.HoughLinesP(edges, 1, np.pi / 180, 10, min_length, max_gap)
     
 
 def detect_strings(img):
+    min_length = 25
+    max_gap = 5
     blur = cv.GaussianBlur(img, (5,5), 0)
     (thresh, img_bw) = cv.threshold(blur, 100, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
-    edges = cv.Sobel(blur, cv.CV_8U, 1, 0, 3)
-    return cv.HoughLines(img_bw, 0.25, np.pi/180, 100)
+    edges = cv.Sobel(img_bw, cv.CV_8U, 0, 1, 3)
+    return cv.HoughLinesP(edges, 1, np.pi / 180, 10, min_length, max_gap)
 
 
-def draw_lines(img, lines):
-    x1_old = y1_old = x2_old = y2_old = 0
-    line_distance = 100
-
-    for line in lines:
-        rho,theta = line[0]
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + 1000*(-b))
-        y1 = int(y0 + 1000*(a))
-        x2 = int(x0 - 1000*(-b))
-        y2 = int(y0 - 1000*(a))
-
-        if abs(x1 - x1_old) > line_distance \
-        and abs(x2 - x2_old) > line_distance \
-        and abs(y2 - y2_old) > line_distance \
-        and abs(y1 - y1_old) > line_distance:
-            cv.line(img, (x1,y1), (x2,y2), (0,0,255), 3)
-        x1_old = x1
-        x2_old = x2
-        y1_old = y1
-        y2_old = y2
+def draw_lines(img, lines, frets=True):
+    for x in range(len(lines)):
+        for x1, y1, x2, y2 in lines[x]:
+            cv.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
     return img
