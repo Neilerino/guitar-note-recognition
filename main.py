7ce4e9a2d6e508adfdd6cb1e2521ad912bb358b8
Jacob <jrw111@mun.ca>
2019-07-22 12:35:44 -0230

    Cleaned up presentation of output display
    - shrinks image to better fit screen
    - ignores the cant find note - notes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fingertip_detector import get_fingertip_coords
 from fretboard.neck import Neck
 from fretboard.fretboard import get_note, get_lines_image
 
+
 def find_fingertip(file_path=None):
     if not file_path:
         file_path = filedialog.askopenfilename()
@@ -15,17 +16,19 @@ def find_fingertip(file_path=None):
     for pair in finger_tip_coords:
         cv.circle(im, pair, 3, (0, 0, 255), 4)
         note = get_note(pair, i_w_lines)
-        cv.putText(im, note, (0, 50), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-        print('Note: ', note)
-    cv.imshow('Fingertip', im)
+        if note != 'Could not identify note':
+            cv.putText(im, note, (0, 50), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+            print('Note: ', note)
+    scaled = cv.resize(im, None, fx=0.5, fy=0.5, interpolation=cv.INTER_AREA)
+    cv.imshow('Note', scaled)
     cv.waitKey(0)
     cv.destroyAllWindows()
 
 
 root = tk.Tk()
-root.geometry("150x50")
+root.geometry("200x50")
 root.resizable(0, 0)
-header = Label(root, text='Note Identifier - v1.0')
+header = Label(root, text='Note Identifier - v1.1')
 header.pack()
 finger_btn = Button(root, text='Get note', command=find_fingertip)
 finger_btn.pack()
